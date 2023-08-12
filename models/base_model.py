c97a54b92ff9eb89efc2e1fef38df128e848e07c
Guy Ahonakpon GBAGUIDI <guygbaguidi123root@gmail.com>
2023-08-12 05:27:31 -0700

    update file

--- models/base_model.py
+++ models/base_model.py
@@ -16,23 +16,20 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """instanciate the class"""
 
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == 'id':
-                    self.id = kwargs[key]
-                elif key == 'created_at':
-                    self.created_at = datetime.strptime(
-                        kwargs[key], "%Y-%m-%dT%H:%M:%S.%f")
-                elif key == 'updated_at':
-                    self.update_at = datetime.strptime(
-                        kwargs[key], "%Y-%m-%dT%H:%M:%S.%f")
-                else:
-                    if key != '__class__':
-                        setattr(self, key, value)
-        else:
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+        time_format = '%Y-%m-%dT%H:%M:%S.%f'
+        if not kwargs:
             self.id = str(uuid.uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
+            models.storage.new(self)
+        else:
+            for key, value in kwargs.items():
+                if key in ("updated_at", "created_at"):
+                    self.__dict__[key] = datetime.strptime(value, time_format)
+                elif key[0] == "id":
+                    self.__dict__[key] = str(value)
+                else:
+                    self.__dict__[key] = value
 
     def __str__(self):
         """print a object"""
